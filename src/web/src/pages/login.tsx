4f8cac099c5eb482cbf840dbfb36188d7481338c
Nuno Gomes <nuno98gomes@gmail.com>
2024-04-11 20:59:17 +0100

    FEAT: Loading Screen refactor + Logo upload

--- src/web/src/pages/login.tsx
+++ src/web/src/pages/login.tsx
@@ -9,7 +9,7 @@ import { Loading } from "../components/loading";
 export function LoginPage() {
   let navigate = useNavigate();
 
-  const { setAuthToken, setUser, setLoading } = useContext(AuthContext);
+  const { setAuthToken, setUser } = useContext(AuthContext);
 
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
@@ -44,7 +44,6 @@ export function LoginPage() {
           setUser(jwtDecode(token));
           localStorage.setItem("authToken", JSON.stringify(token));
           navigate("/dashboard");
-          setLoading(true);
         })
         .catch((error) => {
           console.log(error.message);
@@ -57,25 +56,24 @@ export function LoginPage() {
   };
 
   return (
-    <>
-      {loadingState ? (
-        <Loading />
-      ) : (
-        <>
-          <Header
-            heading="Login to your account"
-            paragraph="Don't have an account yet? "
-            linkName="Signup"
-            linkUrl="/signup"
-          />
+    <div className="bg-gray-200 dark:bg-gray-900 min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <Header
+          heading="Login to your account"
+          paragraph="Don't have an account yet? "
+          linkName="Signup"
+          linkUrl="/signup"
+          isLoading={loadingState}
+        />
+        {!loadingState && (
           <Login
             isError={errorState}
             onSubmit={handleOnSubmit}
             onChange={handleOnChange}
             onFocus={handleOnFocus}
           />
-        </>
-      )}
-    </>
+        )}
+      </div>
+    </div>
   );
 }
