740c4c0f772adcf20e2f54782dd425493f002088
Nuno Gomes <nuno98gomes@gmail.com>
2024-04-12 15:34:31 +0100

    FEAT: Login page refactor error design

--- src/web/src/components/formExtra.tsx
+++ src/web/src/components/formExtra.tsx
@@ -1,3 +1,5 @@
+import { CircleX } from "lucide-react";
+
 interface FormExtraProps {
   error: boolean;
 }
@@ -6,10 +8,12 @@ export function FormExtra(props: FormExtraProps) {
   return (
     <>
       {props.error && (
-        <div>
-          <span className="text-red-500">Email or Password invalid</span>
-        </div>
+        <span className="dark:text-white text-gray-600 flex flex-row gap-2">
+          <CircleX height={15} width={15} color="red" />
+          Email or Password invalid
+        </span>
       )}
+
       <div className="flex items-center justify-between ">
         <div className="flex items-center">
           <input
