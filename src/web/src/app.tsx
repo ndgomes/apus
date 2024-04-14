383dffd7f10e3aa6f727009b536d26cdcd10927d
Nuno Gomes <nuno98gomes@gmail.com>
2024-04-14 21:37:03 +0100

    FIX: fixed sidebar

--- src/web/src/app.tsx
+++ src/web/src/app.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import {
   DashboardPage,
@@ -7,41 +6,23 @@ import {
   QuizPage,
   SignupPage,
 } from "./pages";
-import { Moon, Sun } from "lucide-react";
 import { AuthProvider } from "./context/authContext";
 import RequireAuth from "./utils/requiredAuth";
 
 export function App() {
-  const [dark, setDark] = useState(false);
-
-  const darkModeHandler = () => {
-    setDark(!dark);
-    document.body.classList.toggle("dark");
-  };
-
   return (
-    <>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
 
-          <Route element={<RequireAuth />}>
-            <Route path="/quiz" element={<QuizPage />} />
-            <Route path="/dashboard" element={<DashboardPage />} />
-          </Route>
+        <Route element={<RequireAuth />}>
+          <Route path="/quiz" element={<QuizPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
 
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </AuthProvider>
-
-      <button
-        className="absolute top-8 right-8 border-2 rounded-lg p-2.5 border-purple-600 hover:border-purple-500"
-        onClick={() => darkModeHandler()}
-      >
-        {!dark && <Sun color="white" fill="white" />}
-        {dark && <Moon color="black" fill="black" />}
-      </button>
-    </>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </AuthProvider>
   );
 }
